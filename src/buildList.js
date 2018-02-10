557129eab8df60d02619d2805e8b292c32ce0d8d
darly-r <rpugachoff@gmail.com>
2018-02-10 18:14:56 +0200

    add AJAX request

--- src/buildList.js
+++ src/buildList.js
@@ -1,126 +1,65 @@
 'use strict';
+  	
+function setModalWindowParameters(name, imgSrc, modalBody){
+	document.getElementById('modal-title').innerHTML = name;
+	document.getElementById('book-img').src = imgSrc;
+	document.getElementById('description').innerHTML = modalBody;
+};
+function sortByPrice() {
+    document.getElementById('list-all').innerHTML = '';
+    arrayOfBooks.sort((a, b) => a.price - b.price);
+    viewList(arrayOfBooks);
+};
+function sortByPriceRev() {
+    arrayOfBooks.reverse((a, b) => a.price - b.price);
+    document.getElementById('list-all').innerHTML = '';
+    viewList(arrayOfBooks);
+};
+function sortByID() {
+    arrayOfBooks.sort((a, b) => a.id - b.id);
+    document.getElementById('list-all').innerHTML = '';
+    viewList(arrayOfBooks);
+};
 
-const arrayOfBooks_ = [
-{
-	id: 10001,
-	name: 'Mr. Penumbras 24-Hour Bookstore',
-	author: 'Robin Sloan',
-	price: 38.87,
-	imgSrc: 'images/penumbra.jpg',
-	modalBody: `<p>Farrar, Straus and Giroux, 02/10/2012. 304p.</p> <p>A gleeful and exhilarating tale of global conspiracy, complex code-breaking, high-tech data visualization, young love, rollicking adventure, and the secret to eternal life—mostly set in a hole-in-the-wall San Francisco bookstore</p>`
-},
-{
-	id: 10002,
-	name: 'The Book Thief',
-	author: 'Markus Zusak',
-	price: 8.89,
-	imgSrc: 'images/bookThief.jpg',
-	modalBody: `<p>Hardcover, 552 pages. Published March 14th 2006 by Alfred a Knopf Inc</p> <p>It’s just a small story really, about among other things: a girl, some words, an accordionist, some fanatical Germans, a Jewish fist-fighter, and quite a lot of thievery ...Set during World War II in Germany, Markus Zusak’s groundbreaking new novel is the story of Liesel Meminger, a foster girl living outside of Munich</p>`
-},
-{
-	id: 10003,
-	name: 'Flowers For Algernon',
-	author: 'Daniel Keyesk',
-	price: 5.89,
-	imgSrc: 'images/flowers.jpg',
-	modalBody: `<p>Hardcover, 552 pages. Published March 14th 2006 by Alfred a </p>`
-},
-{
-	id: 10004,
-	name: 'THE THIRTEENTH TALE',
-	author: 'Markus Zusak',
-	price: 29.89,
-	imgSrc: 'images/13thTale.jpg',
-	modalBody: `<p>Hardcover, 552 pages. Published March 14th </p>`
-},
-{
-	id: 10005,
-	name: 'The Girl on the Train',
-	author: 'Paula Hawkins',
-	price: 19.79,
-	imgSrc: 'images/trainGirl.jpg',
-	modalBody: `<p>Hardcover, 552 pages. Published March 14th 2006 by Alfred </p>`
-},
-];
-	
-	function setModalWindowParameters(name, imgSrc, modalBody){
-		document.getElementById('modal-title').innerHTML = name;
-		document.getElementById('book-img').src = imgSrc;
-		document.getElementById('description').innerHTML = modalBody;
-	};
-	function sortByPrice() {
-	    arrayOfBooks.sort((a, b) => a.price - b.price);
-	    document.getElementById('list-all').innerHTML = '';
-	    viewList();
-	};
-	function sortByPriceRev() {
-	    arrayOfBooks.reverse((a, b) => a.price - b.price);
-	    document.getElementById('list-all').innerHTML = '';
-	    viewList();
-	};
-	function sortByID() {
-	    arrayOfBooks.sort((a, b) => a.id - b.id);
-	    document.getElementById('list-all').innerHTML = '';
-	    viewList();
-	};
-	function sortByID() {
-	    arrayOfBooks.sort((a, b) => a.id - b.id);
-	    document.getElementById('list-all').innerHTML = '';
-	    viewList();
-	};
-
-  var book = {publishedDate: '',  	title: '',  	author: '',  	pageCount: ''};
-  var arrayOfBooks = [];
-  for (var i = 0; i < 10; i++) {
-  	arrayOfBooks[i] = book;
-  }
+let publishedDate = '', title = '', author = '', pageCount = '';
+let book = {publishedDate, title, author, pageCount};
+let arrayOfBooks = [];
 
 function loadDoc() {
-  var search = document.getElementById("books").value;
-  var xhttp = new XMLHttpRequest();
-
- 
-
-  document.getElementById("result").innerHTML = '';
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var responseJSON = JSON.parse(this.responseText);
-      for (var i = 0; i < responseJSON.items.length; i++) 
-      {
-       arrayOfBooks[i].publishedDate = responseJSON.items[i].volumeInfo.publishedDate;
-       arrayOfBooks[i].title = responseJSON.items[i].volumeInfo.title;
-       arrayOfBooks[i].author = responseJSON.items[i].volumeInfo.authors;
-       arrayOfBooks[i].pageCount = responseJSON.items[i].volumeInfo.pageCount;
-       }
-
-      viewList(arrayOfBooks);
-    } 
-  };
-  xhttp.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + search, true);
-  xhttp.send();
+  	var search = document.getElementById("books").value;
+  	var xhttp = new XMLHttpRequest();
+  	document.getElementById("result").innerHTML = '';
+  	xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200)	{
+      		var responseJSON = JSON.parse(this.responseText);
+      		for (var i = 0; i < responseJSON.items.length; i++) {
+        		arrayOfBooks[i] = 
+        		{
+        		publishedDate : responseJSON.items[i].volumeInfo.publishedDate,
+       			title : responseJSON.items[i].volumeInfo.title,
+       			author : responseJSON.items[i].volumeInfo.authors,
+       			pageCount : responseJSON.items[i].volumeInfo.pageCount 
+       		};
+      	};    
+		viewList(arrayOfBooks);
+    	};
+    	
+  	};
+  	xhttp.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + search, true);
+  	xhttp.send();    
 }
 
-	function viewList(arrayOfBooks){
-
-		for (let i = 0; i < arrayOfBooks.length; i++) {
-			let classRow = (i % 2 == 0) ? "row bg-light text-dark" : "row bg-secondary text-white";
-			//let name = arrayOfBooks[i].name;
-			let div = document.createElement('div');
-			div.innerHTML = `<div class="${classRow}">
+function viewList(arrayOfBooks){
+	document.getElementById('list-all').innerHTML = '';
+	for (let i = 0; i < arrayOfBooks.length; i++) {
+		let classRow = (i % 2 == 0) ? "row bg-light text-dark" : "row bg-secondary text-white";
+		let div = document.createElement('div');
+		div.innerHTML = `<div class="${classRow}">
 			<div class="col-lg-1">${arrayOfBooks[i].publishedDate}</div>
 			<div class="col-lg-3">${arrayOfBooks[i].title}</div>
 			<div class="col-lg-2">${arrayOfBooks[i].author}</div>
 			<div class="col-lg-1">${arrayOfBooks[i].pageCount}</div>
-			
 			</div>`;
-			document.getElementById('list-all').appendChild(div);
+		document.getElementById('list-all').appendChild(div);
 		};
-	};
-
-
-	//viewList();
-
-	// <div class="col-lg-2">
-	// 		<button type="button" class="btn btn-outline-light text-dark" 
-	// 		data-toggle="modal" data-target="#modal-details" onclick= "setModalWindowParameters('${arrayOfBooks[i].name}', 
-	// 		'${arrayOfBooks[i].imgSrc}', '${arrayOfBooks[i].modalBody}')">View details</button></div>
+};
